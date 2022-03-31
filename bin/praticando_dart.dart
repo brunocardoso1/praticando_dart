f693d76f81f2c18d41935441c1abe3d5ca13129e
Bruno Cardoso <bruno.alvescardoso@hotmail.com>
2022-03-30 22:22:03 -0300

    prac

--- bin/praticando_dart.dart
+++ bin/praticando_dart.dart
@@ -1,8 +1,23 @@
 void main() {
 
+  // var pode ser usavo quando não sabemos o tipo da variavel a ser usado
+  var teste = 0;
 
-  int idade = 21;
+ //todo: teste comentario
 
-  print('eu tenho $idade anos!');
+
+  for(int c=1; c<5; c++){
+    print("repetição numero $c ");
+  }
+
+  int count = 0;
+
+  while(count < 5){
+    print('teste');
+    count = count + 1;
+  }
+
+  // uma lista Dynamic pode receber valores de tipos diferentes
+  List<dynamic> tipos = ['bruno', 23, 23.1, true];
 
 }
